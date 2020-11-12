eee913ea401553e298ee71181f0199d131b16b34
Solveig Grimstad <solveiggrimstad@dhcp-10-220-221-74.wlan.ntnu.no>
2020-11-12 17:48:05 +0100

    likes#9

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -5,6 +5,7 @@ import Routes from "./src/routes";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
+
 export default function App() {
   return (
     <Provider store={store}>
--- my-app/src/MovieInfo.tsx
+++ my-app/src/MovieInfo.tsx
@@ -2,72 +2,80 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, ScrollView, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { IMovie } from "./Movies";
-import { Card, Title, Paragraph } from "react-native-paper";
+import { Card, Title, Paragraph, Divider } from "react-native-paper";
 import { Entypo } from "@expo/vector-icons";
 import axios from "axios";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function MovieInfo(props: any) {
   const movie = useRoute().params as IMovie;
-  const [hearts, setHearts] = useState<number>(props.starRating);
   const [like, setLike] = useState<number>(0);
-  //likebutton
+  const [color, setColor] = useState(false);
 
-  useEffect(() => {
-    setHearts(props.starRating);
-  }, []);
+  //likebutton
 
   const handleClick = () => {
     //when liking, changing color and the count decreases/increases
     setColor(!color);
 
     if (color == true) {
-      axios.put(`http://it2810-42.idi.ntnu.no:8000/api/dislike/${props.id}`);
+      axios.put(`http://it2810-42.idi.ntnu.no:8000/api/dislike/${movie._id}`);
 
-      setHearts(hearts - 1);
       setLike(like - 1);
     } else {
-      axios.put(`http://it2810-42.idi.ntnu.no/api/like/${props.id}`);
-      setHearts(hearts + 1);
+      axios.put(`http://it2810-42.idi.ntnu.no:8000/api/like/${movie._id}`);
+
       setLike(like + 1);
     }
   };
 
-  const [color, setColor] = useState(false);
-
-  return (
+c  return (
     <ScrollView>
       <Card style={styles.card}>
         <Card.Content>
-          <Title>{movie.Title}</Title>
-          <View style={styles.yearHeart}>
-            <Paragraph>
+          <View style={styles.styleTitle}>
+            <Title>{movie.Title}</Title>
+          </View>
+          <Divider />
+          <View style={styles.likeContainer}>
+            <Text style={styles.year}>
               {movie.Year}
               {"\n"}
-            </Paragraph>
+            </Text>
 
-            <MaterialCommunityIcons
-              name="cards-heart"
-              size={40}
-              isRed={color}
-              onPress={() => handleClick()}
-              style={styles.heartButton}
-            />
+            <Text style={styles.star}>
+              {movie.Ratings} <Entypo name="star" size={24} color="#ffca28" />
+            </Text>
           </View>
 
           <Card.Cover style={styles.cardImage} source={{ uri: movie.Poster }} />
-          <Text>
-            {movie.Ratings} <Entypo name="star" size={24} color="#ffca28" />
-          </Text>
-          <Text>
-            Likes on this movie: {"\n"} {(props.starRating + like).toString()}
-          </Text>
+
+          <View style={styles.likeContainer}>
+            <View style={styles.likeHeart}>
+              <Text>
+                Likes on this movie: {"\n"}{" "}
+                {(movie.starRating + like).toString()}
+              </Text>
+            </View>
+            <View style={styles.heartButton}>
+              <MaterialCommunityIcons
+                name="cards-heart"
+                size={40}
+                color={color ? "red" : "black"}
+                onPress={() => handleClick()}
+              />
+            </View>
+          </View>
+
+          <Divider />
 
           <Text>
             {"\n"}
             {movie.Plot}
           </Text>
         </Card.Content>
+        <Divider />
       </Card>
     </ScrollView>
   );
@@ -83,9 +91,33 @@ const styles = StyleSheet.create({
   },
   yearHeart: {
     flexDirection: "row",
+    backgroundColor: "white",
   },
   heartButton: {
-    textAlign: "left",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginLeft: 150,
+  },
+  styleTitle: {
+    flexDirection: "row",
+  },
+  likeHeart: {},
+  heart: {
+    alignContent: "space-between",
+  },
+  likeContainer: {
+    flexDirection: "row",
+  },
+  star: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginLeft: 70,
+    backgroundColor: "white",
+    fontWeight: "bold",
+  },
+  year: {
+    marginTop: 7,
+    fontWeight: "bold",
   },
 });
 
